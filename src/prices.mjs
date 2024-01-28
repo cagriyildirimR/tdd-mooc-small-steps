4525cfbbef265b0fa748396470daef591c6a913d
Cagri Yildirim <cagri.yildirim.r@gmail.com>
2024-01-28 15:41:50 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -87,6 +87,7 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
+      let holiday2 = parseTDate(row.holiday);
       if (
         date && dateT &&
         date.getFullYear() === holiday.getFullYear() &&
