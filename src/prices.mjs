c43428ab8e5a4aa881d9d07ee5bca71238d9a859
Cagri Yildirim <cagri.yildirim.r@gmail.com>
2024-01-28 15:49:02 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -21,7 +21,7 @@ function createApp(database) {
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
     const dateT = parseTDate(req.query.date);
-    const cost = calculateCost(age, type, dateT, baseCost, dateT);
+    const cost = calculateCost(age, type, dateT, baseCost);
     res.json({ cost });
   });
 
